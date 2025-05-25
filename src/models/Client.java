6a1434720f581bada567e24990f00b7b485a54c4
Mescxll <202411190002@ifba.edu.br>
2025-05-25 18:49:26 -0300

    Adicionando models

---
+++ src/models/Client.java
@@ -0,0 +1,65 @@
+package models;
+
+import java.time.LocalDate;
+
+public class Client {
+    private static int _idGenerator = 1;
+    private int id;
+    private String name;
+    private String email;
+    private LocalDate birthday;
+    private CircularDinamicList<Ticket> purchasingHistory;
+    
+    public Client(String name, String email, LocalDate birthday) {
+        this.name = name;
+        this.email = email;
+        this.birthday = birthday;
+        purchasingHistory = new CircularDinamicList<Ticket>();
+        id = _idGenerator++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public CircularDinamicList<Ticket> getPurchasingHistory() {
+        return purchasingHistory;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String printHistory(){
+        String result = "";
+        for (int i = 0; i < purchasingHistory.size(); i++) {
+            result += purchasingHistory.get(i).toString() + "\n";
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Client " + id + ":" + 
+        "\nName= " + name + 
+        "\nEmail= "+ email + 
+        "\nBirthday= "+ birthday + 
+        "\nPurchasing History= " + purchasingHistory;
+    }   
+}
