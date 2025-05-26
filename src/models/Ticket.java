8bff6520d743cdfb19cb5927702ae758ee646eb2
sunHelen12 <sunrise.developer12@gmail.com>
2025-05-25 22:33:10 -0300

    Class Ticket

---
+++ src/models/Ticket.java
@@ -0,0 +1,48 @@
+package models;
+
+public class Ticket {
+
+    private static int _idGenerator = 1;
+    private int id;
+    private Client client;
+    private Session session;    
+    private PaymentMethod paymentMethod;
+
+    public Ticket(Client client, Session session, PaymentMethod paymentMethod, int id) {        
+        this.client = client;
+        this.session = session;
+        this.paymentMethod = paymentMethod;
+        this.id = _idGenerator++;
+    }
+
+    public static int get_idGenerator() {
+        return _idGenerator;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+      @Override
+    public String toString() {
+        return "Ticket " + id + ":" + 
+               "\nClient= " + client.getName() + 
+               "\nSession= " + session.getMovie().getTitle() + 
+               "\nDate= " + session.getDate() + 
+               "\nTime= " + session.getTime() + 
+               "\nPayment Method= " + paymentMethod;
+    }    
+    
+}
