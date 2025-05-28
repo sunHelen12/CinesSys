9451c6167f6f9418b97f5333b2b6fe5757cbc75f
Mescxll <202411190002@ifba.edu.br>
2025-05-28 15:18:59 -0300

    Adicionando controle para OccupationRelatory

---
+++ src/controller/viewcontroller/MainOccupationRoom.java
@@ -0,0 +1,28 @@
+package controller.viewcontroller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainOccupationRoom extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/gui/OccupationRelatory.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Ocupação de Sala");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
+
+//compilar
+// javac -d out --module-path "C:\javafx-sdk-21.0.7\lib" --add-modules javafx.controls,javafx.fxml src/controller/viewcontroller/MainOccupationRoom.java
+//executar
+// java --module-path "C:\javafx-sdk-21.0.7\lib" --add-modules javafx.controls,javafx.fxml -cp "out;src" controller.viewcontroller.MainOccupationRoom
