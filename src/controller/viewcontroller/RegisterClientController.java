b65dbf15ece56083481c17f3501911e475e86fe4
Mescxll <202411190002@ifba.edu.br>
2025-06-12 11:35:19 -0300

    Lóciga e Documentação do Registro de Clientes

--- src/controller/viewcontroller/RegisterClientController.java
+++ src/controller/viewcontroller/RegisterClientController.java
@@ -8,23 +8,40 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 import controller.viewcontroller.MainViews;
-import repository.*;
+import controller.viewcontroller.ClientControlController;
+import services.*;
 
+/**
+ * Classe referente ao controle da interface "Registrar Clientes" presente na
+ * aplicação.
+ * 
+ * @author Maria Eduarda Campos
+ * @since 31-05-2025
+ * @version 2
+ */
 public class RegisterClientController {
     @FXML
     private TextField enterDate;
-
     @FXML
     private TextField enterEmail;
-
     @FXML
     private TextField enterName;
     
+    /**
+     * Método que abre a Tela de Controle de Clientes.
+     * 
+     * @param event Evento ao apertar o botão, caso necessário.
+     */
     @FXML
     void backClient(ActionEvent event) {
         MainViews.changeScreen("clientControl", null);
     }
 
+    /**
+     * Método que registra um novo Cliente.
+     * 
+     * @param event Evento ao apertar o botão, caso necessário.
+     */
     @FXML
     void registerClient(ActionEvent event) {
         String name = enterName.getText().trim();
@@ -33,8 +50,9 @@ public class RegisterClientController {
 
         if (name.isEmpty() || email.isEmpty() || date.isEmpty()) {
             return;
+        }else{
+            ClientService.addClient(name, email, date);
+            ClientControlController.mostrarPopUp("cadastrado");
         }
-
-        //Cadastrar Cliente
     }
 }
