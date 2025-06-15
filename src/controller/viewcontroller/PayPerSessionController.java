75e160cab29d53f8e8e8f1e865c765966bcc8b39
Vinícius <viniciusandradebc@gmail.com>
2025-06-14 21:06:53 -0300

    Adicionando PayPerSessionController

---
+++ src/controller/viewcontroller/PayPerSessionController.java
@@ -0,0 +1,43 @@
+package controller.viewcontroller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class PayPerSessionController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Label dateTime;
+
+    @FXML
+    private Label freeSeatsTotalSeats;
+
+    @FXML
+    private Label mapSeats;
+
+    @FXML
+    private Label movieDuration;
+
+    @FXML
+    private Label movieName;
+
+    @FXML
+    private Label soldTickets;
+
+    @FXML
+    private Label ticketValue;
+
+    @FXML
+    private Label valueCollected;
+
+    @FXML
+    void voltar(ActionEvent event) {
+
+    }
+
+}
+
